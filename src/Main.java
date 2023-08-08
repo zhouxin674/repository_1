fed8dcf8117344aae755c8091bf3fc37a2a62d54
周鑫 <2805657660@qq.com>
2023-08-08 17:49:44 +0800

    test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author 周鑫
+ * @version 1.0
+ * @date 2023-08-08 17:43
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/zx/Hello.java
@@ -0,0 +1,13 @@
+package com.zx;
+
+/**
+ * @author 周鑫
+ * @version 1.0
+ * @date 2023-08-08 17:43
+ * @Description
+ */
+public class Hello {
+ public static void main(String[] args) {
+  System.out.println("hello world!!!");
+ }
+}
